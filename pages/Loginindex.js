a4ea2431360824393eefa9dda28ca3584c5a4139
Aarti Bagwan <aartibagwan67@gmail.com>
2024-04-04 14:19:06 +0530

    third commit

---
+++ pages/Loginindex.js
@@ -0,0 +1,12 @@
+import LoginPage from '@/comopents/LoginPage'
+import React from 'react'
+
+const Loginindex = () => {
+  return (
+    <div>
+      <LoginPage/>
+    </div>
+  )
+}
+
+export default Loginindex
